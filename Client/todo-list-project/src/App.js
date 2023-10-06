5624609807928f127f517fbd0a4c2e349b4cbf89
Ariel Claros <Ariel.Claros@fundacion-jala.org>
2023-10-06 08:12:22 -0400

    Add the frontend connected with the backend

--- Client/todo-list-project/src/App.js
+++ Client/todo-list-project/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './TaskList.css'; // Importa el archivo de estilos
+import TaskList from './Components/TaskList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome TodoList
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-list">
+      <TaskList />
     </div>
   );
 }
